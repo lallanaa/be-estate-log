2a5b06e6c9a901a0d80ee483b3ab9e7ce24fe470
sungheena <na.sunghee1@gmail.com>
2025-12-29 20:48:41 +0900

    chore: initial project setup

---
+++ src/test/java/com/kyungsung/estate/EstateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyungsung.estate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
